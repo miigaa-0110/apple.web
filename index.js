4a361e6b68045d16d0c5fb15f91ed6b4cb91bdf5
miigaa-0110 <myagmarsurendavaadagvaa@gmail.com>
2026-03-31 09:01:34 +0800

    finished

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+let number = 10;
+
+let number1 = 10;
+let number2 = 30;
+let number3 = 40;
+console.log(number1 + number2 + number3);
+
+let fistname = "myagmarsuren";
+let lastname = "davaadagva";
+let age = 30;
+let gender = "female";
+let phonenumber = 88939221;
+let email = "maygmarsurendavaadagvaa@gmail.com";
+
+let a = 5;
+a = a * a + a;
+console.log(a);
+
+let b = 5;
+b = b / b - b;
+console.log(b);
+
+let c = 10;
+let s = 12;
+let d = 15;
+console.log((c + s + d) * 2);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let fullname = firstname + lastname;
+
+let number1 = 10;
+let number2 = 30;
+
+console.log();
